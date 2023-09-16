22669d17f0facd49a6e7fe8d2779c4caa811fc0f
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-16 03:51:12 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, list):
+        return [word for word in list if sorted(word.upper()) == sorted(self.word.upper())]
+
+Listen = Anagram("listen")
+print(Listen.match(["enlists", "google", "fatty", "inlets"]))
